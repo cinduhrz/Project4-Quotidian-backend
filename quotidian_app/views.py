d4ec1cdfa63e10799d5d40c29762745835ebaeb4
Cindy Wong <cindywongdev@gmail.com>
2023-02-22 16:15:36 -0500

    added TodoViewSet

--- quotidian_app/views.py
+++ quotidian_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Todo
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TodoSerializer
 
 # Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    # The Main Query for the index route
+    # pass the model
+    queryset = Todo.objects.all()
+    # pass the serializer
+    serializer_class = TodoSerializer
+    # define permission class (optional)
+    permission_classes = [permissions.AllowAny]

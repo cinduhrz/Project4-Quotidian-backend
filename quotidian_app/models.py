7adc8d159e4bf365e566357389adabe74d7c9268
Cindy Wong <cindywongdev@gmail.com>
2023-02-22 16:05:00 -0500

    created todo model

--- quotidian_app/models.py
+++ quotidian_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    # auto sets date field to when object is first created
+    date = models.DateField(auto_now_add=True)
+    
+    # actual text of todo
+    description = models.CharField(max_length=100)
+    
+    # todo can fall into 3 categories: event, task, and reminder (backburner tasks)
+    category = models.CharField(max_length=25)
+    
+    # status only exists for todos in the task category, and there are 3 possible values: unfinished, finished, and cancelled (this doesn't remove the todo from the database, unlike deleting it does. it simply crosses it out).
+    # for todos in event and reminder category, status will be null
+    status = models.CharField(max_length=25, null=True)
